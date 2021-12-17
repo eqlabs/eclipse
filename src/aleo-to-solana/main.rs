edbfa528853669c055f024f0ca56e1a59d3eb3c3
bwty <whalelephant@users.noreply.github.com>
2021-12-17 21:57:15 +0000

    fix: use forked json-rpc-type

--- src/aleo-to-solana/main.rs
+++ src/aleo-to-solana/main.rs
@@ -3,9 +3,9 @@ use jsonrpsee::{http_client::HttpClientBuilder, types::traits::Client};
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // Assuming that the snarkOS is running locally, by default 3032 is the rpc server port
-    let url = format!("http://{}", "0.0.0.0:3032");
+    let url = format!("http://{}", "127.0.0.1:3032");
     let client = HttpClientBuilder::default().build(url)?;
-    let response: Result<String, _> = client.request("latestblock", None).await;
+    let response: Result<serde_json::Value, _> = client.request("latestblock", None).await;
     println!("response: {:?}", response);
     Ok(())
 }
@@ -26,10 +26,10 @@ mod tests {
         // Print out from the Aleo node receiving the request before parsing
         // b"{\"jsonrpc\":\"2.0\",\"id\":0,\"method\":\"getblock\"}";
         let aleo_req: Request<Params> =
-            serde_json::from_slice(&from_eclipse_str.as_bytes()).unwrap();
+            serde_json::from_slice(from_eclipse_str.as_bytes()).unwrap();
 
         let aleo_req_str = serde_json::to_string(&aleo_req).unwrap();
-        let parsed_aleo_req: SeeRequest = serde_json::from_slice(&aleo_req_str.as_bytes()).unwrap();
+        let parsed_aleo_req: SeeRequest = serde_json::from_slice(aleo_req_str.as_bytes()).unwrap();
 
         assert_eq!(parsed_aleo_req.method, from_eclipse.method);
         assert_eq!(parsed_aleo_req.id, from_eclipse.id);
