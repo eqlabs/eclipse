7b6987927f4fdddeebc606ebbfb5ce098debafb2
bwty <whalelephant@users.noreply.github.com>
2022-01-10 15:05:35 +0100

    chore: clean up irrelevant tests and deps

--- src/aleo-to-solana/main.rs
+++ src/aleo-to-solana/main.rs
@@ -153,39 +153,3 @@ async fn process_block(block: &Block<Testnet2>, client: &HttpClient) -> anyhow::
 
     Ok(())
 }
-
-#[cfg(test)]
-mod tests {
-    use json_rpc_types::Request;
-    use jsonrpc_core::Params;
-    use jsonrpsee_types::v2::{Id, Request as SeeRequest, RequestSer};
-    use serde_json;
-
-    #[test]
-    fn it_serde() {
-        let id = Id::Number(0);
-        let from_eclipse = RequestSer::new(id, "latestblock", None);
-        let from_eclipse_str =
-            serde_json::to_string(&from_eclipse).expect("value constructed above can't fail");
-
-        // Print out from the Aleo node receiving the request before parsing
-        // b"{\"jsonrpc\":\"2.0\",\"id\":0,\"method\":\"getblock\"}";
-        let aleo_req: Request<Params> = serde_json::from_slice(from_eclipse_str.as_bytes())
-            .expect("from_eclipse_str constructed above");
-
-        let aleo_req_str = serde_json::to_string(&aleo_req).expect("request constructed above");
-        let parsed_aleo_req: SeeRequest = serde_json::from_slice(aleo_req_str.as_bytes())
-            .expect("aleo_req_str constructed above");
-
-        assert_eq!(parsed_aleo_req.method, from_eclipse.method);
-        assert_eq!(parsed_aleo_req.id, from_eclipse.id);
-        assert_eq!(
-            parsed_aleo_req.params.is_none(),
-            from_eclipse.params.is_none()
-        );
-        assert_eq!(
-            serde_json::to_string(&aleo_req).expect("aleo_req constructed above"),
-            from_eclipse_str
-        );
-    }
-}
